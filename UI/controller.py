b4c114c5bd77b99c5dcd4a021325ffcbb5215530
federicataverriti <taverritifederica@gmail.com>
2026-05-18 10:04:21 +0200

    Lezione 18/05/2026

--- UI/controller.py
+++ UI/controller.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import flet as ft
 
 
@@ -51,11 +52,77 @@ class Controller:
 
 
     def handleConnessi(self,e):
-        pass
+        if self._choicePartenza is None:
+            self._view._txtResults.controls.clear()
+            self._view._txtResults.controls.append(
+                ft.Text("Attenzione, per usare questo metodo occorre selezionare un aeroporto di partenza"))
+            self._view.update_page()
+            return
+
+        viciniT = self._model.getViciniOrdinati(self._choicePartenza)
+        self._view._txtResults.controls.clear()
+        for v in viciniT:
+            self._view._txtResults.controls.append(
+                ft.Text(f"{v[0]} - peso: {v[1]}"))
+        self._view.update_page()
+
+    def handleTestConnessione(self,e):
+        #verificare connessione esiste, se esiste trovare cammino qualsiasi
+        if self._choicePartenza is None:
+            self._view._txtResults.controls.clear()
+            self._view._txtResults.controls.append(
+                ft.Text("Attenzione, per usare questo metodo occorre selezionare un aeroporto di partenza", color="red"))
+            self._view.update_page()
+            return
+
+        if self._choiceArrivo is None:
+            self._view._txtResults.controls.clear()
+            self._view._txtResults.controls.append(
+                ft.Text("Attenzione, per usare questo metodo occorre selezionare un aeroporto di arrivo", color="red"))
+            self._view.update_page()
+            return
+
+        if not self._model.hasPath(self._choicePartenza, self._choiceArrivo):
+            self._view._txtResults.controls.clear()
+            self._view._txtResults.controls.append(
+                ft.Text(f"Non ho trovato un cammino tra {self._choicePartenza} e {self._choiceArrivo}", color="orange"))
+            self._view.update_page()
+            return
+
+        path = self._model.getPath(self._choicePartenza, self._choiceArrivo)
+        self._view._txtResults.controls.clear()
+        self._view._txtResults.controls.append(
+            ft.Text(f"Ho trovato un cammino tra {self._choicePartenza} e {self._choiceArrivo}"
+                    f"Di seguito i nodi che compongono il cammino", color="green"))
+
+        for p in path:
+            self._view._txtResults.controls.append(ft.Text(p))
+        self._view.update_page()
 
     def handleCerca(self,e):
-        pass
+        t = self._view._txtInNTratteMax.value
 
+        try:
+            tInt = int(t)
+        except ValueError:
+            self._view._txtResults.controls.clear()
+            self._view._txtResults.controls.append(
+                ft.Text("Il valore di t deve essere un intero positivo.", color="red"))
+            return
+
+        tic = datetime.now()
+        path, score = self._model.getCamminoOttimo(self._choicePartenza, self._choiceArrivo, tInt)
+        self._view._txtResults.controls.clear()
+        self._view._txtResults.controls.append(
+            ft.Text(f"Cammino tra {self._choicePartenza} e {self._choiceArrivo} trovato", color="green"))
+        self._view._txtResults.controls.append(
+            ft.Text(f"Il cammino ha uno score complessivo pari a {score} e contiene i seguenti nodi:", color="green"))
+
+        for p in path:
+            self._view._txtResults.controls.append(ft.Text(p), color="green")
+            self._view._txtResults.controls.append(
+                ft.Text(f"Cammino trovato in {datetime.now-tic}", color="green"))
+        self._view.update_page()
 
     def _fillDropdown(self,allNodes):
         for n in allNodes:
--- model/model.py
+++ model/model.py
@@ -1,3 +1,6 @@
+import copy
+from stringprep import b1_set
+
 import networkx as nx
 
 from database.DAO import DAO
@@ -10,6 +13,43 @@ class Model:
         self._idMapAirports = {} #mappa tra id aereoporti e aeroporti
         for a in self._airports:
             self._idMapAirports[a.ID] = a
+
+        self._bestCammino = []
+        self._bestScore = 0
+
+
+    def getCamminoOttimo(self, v0, v1, t):
+        self._bestCammino = []
+        self._bestScore = 0
+
+        parziale = [v0]
+        self._ricorsione(parziale, v1, t)
+
+        return self._bestCammino, self._bestScore
+
+    def ricorsione(self, parziale, v1, t):
+        #verifico se parziale è una soluzione valida, ed in caso la salvo
+        if parziale[-1] == v1:#allora arrivato ad aeroporto di destinazione; potenzialmente sol accettabile
+            if self._getScore(parziale) > self._bestScore:
+                self._bestCammino = copy.deepcopy(parziale)
+                self._bestScore = self._getScore(parziale)
+
+        #verifico se ha senso continuare ad aggiungere elementi in parziale, oppure esco
+        if len(parziale) == t+1: #allora parziale ha già raggiunto il numero massimo di tratte, non posso andare avanti
+            return
+
+        #espando parziale e faccio ricorsione con backtracking
+        for n in self._graph.neighbors(parziale[-1]):
+            if n not in parziale:
+                parziale.append(n)
+                self._ricorsione(parziale, v1, t)
+                parziale.pop()
+
+    def getScore(self, parziale):
+        sumPesi = 0;
+        for i in range(0, len(parziale)-1): #cicliamo su archi
+            sumPesi += self._graph[parziale[i]][parziale[i+1]]["weight"]
+        return sumPesi
 
     def buildGraph(self, nMin):
         nodes = DAO.getAllNodes(nMin, self._idMapAirports)
@@ -38,6 +78,46 @@ class Model:
         for t in allTratte:
             if t.aeroportoP in self._graph and t.aeroportoA in self._graph:
                 self._graph.add_edge(t.aeroportoP, t.aeroportoA, weight=t.peso)
+
+    def getViciniOrdinati(self, source):
+        #restituidce tutti i vicini di source, ordinati per peso dell'arco che collega
+        vicini = self._graph.neighbors(source)
+        viciniT = []
+        for v in vicini:
+            viciniT.append((v, self._graph[source][v]["weight"]))
+        viciniT.sort(key=lambda x: x[1], reverse=True)
+        return viciniT
+
+    def hasPath(self, v0, v1):
+        #restituisce true se esiste cammino tre v0 e v1, altrimenti restituisce false
+        return v1 in nx.node_connected_component(self._graph, v0) #componente connessa che contiene v0
+
+    def getPath(self, v0, v1):
+        #esplorare il grafo per esempio con bfs;
+        #per ogni nodo (key) mi dice il nodo precedente nell'albero di visita
+        #V1
+        #dictOfPredecessors = dict(nx.bfs_predecessors(self._graph, v0))
+        #path = [v1]
+        #while path[0] != v0:
+            #path.insert(0, dictOfPredecessors[path[0]])
+        #path = [v0, ---- , v1] (parto dal fondo, poi metto predecessore di v1, poi predecessore del predecessore fino ad arrivare a v0)
+
+        #V2
+        #dictOfPredecessors = dict(nx.dfs_predecessors(self._graph, v0))
+        #path = [v1]
+        #while path[0] != v0:
+            #path.insert(0, dictOfPredecessors[path[0]])
+
+        #V3
+        #path = nx.shortest_path(v0, v1)
+
+        #V4
+        #path = nx.dijkstra_path(self._graph, v0, v1, weight = None) #cammino minimo non guardando i pesi
+
+        #V5
+        path = nx.dijkstra_path(self._graph, v0, v1)
+        return path
+
 
     def getGraphDetails(self):
         return len(self._graph.nodes), len(self._graph.edges)
